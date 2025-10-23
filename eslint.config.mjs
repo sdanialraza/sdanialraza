0e4bcfc455cf060f34696a1811c56a99b16cf3ca
Danial Raza <danialrazafb@gmail.com>
2025-10-23 23:07:31 +0200

    chore: update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import browser from "eslint-config-neon/browser";
+import common from "eslint-config-neon/common";
+import react from "eslint-config-neon/react";
+import node from "eslint-config-neon/node";
+import next from "eslint-config-neon/next";
+import prettier from "eslint-config-neon/prettier";
+import edge from "eslint-config-neon/edge";
+import typescript from "eslint-config-neon/typescript";
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const config = [
+  {
+    ignores: ["ecosystem.config.cjs", "eslint.config.js", "node_modules/**", "dist/**"],
+  },
+  ...common,
+  ...browser,
+  ...node,
+  ...typescript,
+  ...react,
+  ...next,
+  ...edge,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  ...prettier,
+  pluginPrettierRecommended,
+];
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 export default {
-  experimental: {
-    ppr: true,
-    reactCompiler: true,
-  },
   logging: {
     fetches: {
       fullUrl: true,
